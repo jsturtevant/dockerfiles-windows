3d7f9f3136ea948c2ab36b71b284f95d6acfea7e
James Sturtevant <jsturtevant@gmail.com>
2018-03-29 16:52:06 -0700

    fix typo

--- vs-debug/Start.ps1
+++ vs-debug/Start.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 $ProgressPreference = 'SilentlyContinue';
 
 Write-Host "Checking for volume.."
-if((Get-ChildItem C:\debuger -Force | Select-Object -First 1 | Measure-Object).Count -eq 0)
+if((Get-ChildItem C:\debugger -Force | Select-Object -First 1 | Measure-Object).Count -eq 0)
 {
    echo "Empty data volume detected. Populating with debugger.."
    cp -r "C:\Program Files\Microsoft Visual Studio 15.0\Common7\IDE\Remote Debugger\x64\\*" C:\debuger
