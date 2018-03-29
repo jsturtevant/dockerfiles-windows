cf893b7b175e1cc3012baef39f7ef7eaeae5b10e
James Sturtevant <jsturtevant@gmail.com>
2018-03-29 12:35:30 -0700

    fix for full name resolution in k8 (and else where)

--- whoami-iis/whoami-iis/Controllers/HomeController.cs
+++ whoami-iis/whoami-iis/Controllers/HomeController.cs
@@ -11,10 +11,16 @@ namespace whoami_iis.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.MachineName = HttpContext.Server.MachineName;
-            ViewBag.HostName = Request.Url.Host;
+            // There are several ways to get the Machine Name
+            // Using Dns.GetHostName will resolve the full 
+            // name given.  In Kuberentes this will be the Pod Name
+            // Replication ID and the Container Id.  Other options only 
+            // print the Pod and Replications IDs.
+
+            // ViewBag.MachineName = HttpContext.Server.MachineName;
+            // ViewBag.HostName = Request.Url.Host;
             ViewBag.DnsHostName = Dns.GetHostName();
-            ViewBag.EnvHostName = System.Environment.MachineName;
+            // ViewBag.EnvHostName = System.Environment.MachineName;
             return View();
         }
     }
